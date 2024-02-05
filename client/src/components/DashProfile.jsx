f86cd3f2996634023995dcce045d85e64406407b
OreaRoros <orearrs@gmail.com>
2024-02-05 14:01:37 -0800

    complete profile page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import { Button, TextInput } from "flowbite-react";
+import { useSelector } from "react-redux";
 
 export default function DashProfile() {
-  return <div>DashProfile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img
+            src={currentUser.avatar}
+            alt="user profile"
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput type="emaul" id="email" defaultValue={currentUser.email} />
+        <TextInput type="password" id="password" placeholder="********" />
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Modifier
+        </Button>
+      </form>
+      <div className="flex justify-between mt-5 text-red-500">
+        <span className="cursor-pointer">Supprimer</span>
+        <span className="cursor-pointer">Se déconnecter</span>
+      </div>
+    </div>
+  );
 }
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -24,6 +24,7 @@ export default function DashSidebar() {
               label={"User"}
               labelColor="dark"
               className="cursor-pointer"
+              as="div"
             >
               Profile
             </Sidebar.Item>
